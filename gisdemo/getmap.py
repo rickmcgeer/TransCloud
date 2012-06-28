9ef93efcfe1910f476e457f519834a8e0c63833d
Christopher Matthews <cmatthew@Christophers-MacBook-Air.local>
2012-06-27 23:51:18 -0700

    prototype wms getter and colour dector

--- gisdemo/getmap.py
+++ gisdemo/getmap.py
@@ -1,20 +1,46 @@
 from owslib.wms import WebMapService
+import png
+box = (-123.57411,48.3106147, -123.20006,48.6980295)
 wms = WebMapService('http://ows.geobase.ca/wms/geobase_en', version='1.1.1')
 print wms.identification.type
 print wms.identification.version
 print wms.identification.title
 wms.identification.abstract
 print list(wms.contents)
-
+print wms['imagery:landsat7'].styles
 
 img = wms.getmap(   layers=['imagery:landsat7'],
-                    styles=['visual_bright'],
+                    styles=[],
                     srs='EPSG:4326',
-                     bbox=(-112, 36, -106, 41),
+                     bbox=box,
                      size=(300, 250),
-                     format='image/jpeg',
+                     format='image/png',
                      transparent=True
                      )
-out = open('jpl_mosaic_visb.jpg', 'wb')
-out.write(img.read())
-out.close()
+
+bytes_of_png = img.read()
+
+
+image = png.Reader(bytes=bytes_of_png)
+
+rgbs = list(image.asRGBA()[2])
+pixels = 0
+rs = 0
+bs = 0
+gs = 0
+for row in rgbs:
+    for pixel in row[0::4]:
+        pixels += 1
+        if pixel > 128:
+            rs += 1
+    for pixel in row[1::4]:
+        pixels += 1
+        if pixel > 128:
+            gs += 1
+    for pixel in row[2::4]:
+        pixels += 1
+        if pixel > 128:
+            bs += 1
+
+
+print pixels, rs, bs, gs
