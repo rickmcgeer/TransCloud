01f30465ebc15c2ea085fd873bd26aa2e4999e25
Chris Matthews <chris4000@gmail.com>
2012-08-19 16:50:48 -0700

    made a swift crawler

---
+++ gisdemo/list_swift.py
@@ -0,0 +1,42 @@
+import subprocess
+
+command = ["swift", "-A",
+           "https://swift.gcgis.trans-cloud.net:8080/auth/v1.0",
+           "-U",
+           "system:gis",
+           "-K",
+           "uvicgis"]
+containers = list(command)
+containers.append("list")
+p = subprocess.Popen(containers, stdout=subprocess.PIPE)
+out, err = p.communicate()
+print out,err
+
+to_crawl = []
+for line in out.split("\n"):
+    if line[0] == "p":
+        print line
+        new_command = list(containers)
+        new_command.append(line)
+        p = subprocess.Popen(new_command, stdout=subprocess.PIPE)
+        out, err = p.communicate()
+        print out
+        for files in out.split("\n"):
+            to_crawl.append((line, files))
+
+print to_crawl
+to_crawl[:] = [f for f in to_crawl if f != ""]
+
+for files in to_crawl:
+    
+    download_command = list(command)
+    download_command.append("download")
+    download_command.append(files[0])
+    download_command.append(files[1])
+    print download_command
+    #p = subprocess.Popen(new_command, stdout=subprocess.PIPE)
+    #out, err = p.communicate()
+            
+
+
+
