8591ae385d901b350c8027b16d1811b0669352f1
Chris Matthews <chris4000@gmail.com>
2012-08-20 15:25:44 -0700

    url updates

--- gisdemo/landsat.py
+++ gisdemo/landsat.py
@@ -5,16 +5,17 @@ import threading
 import time
 import decimal
 import threadpool
+import sys
 
 
 class LandsatFile:
+
     def _get_pathparts(self, filepath):
         parts = self.filename.split('_')
-        self.pathrow =  parts[0]
+        self.pathrow = parts[0]
         dateband = parts[1].split('.SR.')
         self.date = dateband[0]
         self.band = dateband[1][:-7]
-
 
     def __init__(self, path, size):
         self.path = path
@@ -24,33 +25,37 @@ class LandsatFile:
 
     def __str__(self):
         return self.pathrow + " " + self.date + " " + self.band
- 
 
 
-
-def crawl_filesystem(searchpath, pattern = "*.gz"):
+def crawl_filesystem(searchpath, pattern="*.gz"):
     for path in searchpath:
         for subpath, subdirs, files in os.walk(path):
-            for match in glob.glob(os.path.join(subpath,pattern)):
+            for match in glob.glob(os.path.join(subpath, pattern)):
                 yield match
 
+
 def sizeof_fmt(num):
-    for x in ['bytes','KB','MB','GB','TB']:
+    for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
         if num < 1024.0:
             return "%3.1f%s" % (num, x)
         num /= 1024.0
 
+
 def filter(path):
     filename = os.path.basename(path)
     if '.SR.b' in filename:
-        if '.SR.b07' in filename or '.SR.b04' in filename or '.SR.b03' in filename:
+        if '.SR.b07' in filename \
+               or '.SR.b04' in filename \
+               or '.SR.b03' in filename:
             return True
     return False
 
-# https://198.55.37.2:8080/auth/v1.0 -U system:gis -K uvicgis upload myfiles bigfile2.tgz
+
 def upload_single_landsat(lsat):
-    command = ["swift", "-A",
-               "https://swift.gcgis.trans-cloud.net:8080/auth/v1.0",
+    program = "echo"
+
+    command = [program, "-A",
+               "http://swift.gcgis.trans-cloud.net:8080/auth/v1.0",
                "-U",
                "system:gis",
                "-K",
@@ -59,16 +64,12 @@ def upload_single_landsat(lsat):
                "-c",
                lsat.pathrow, lsat.filename]
     # command = ["ln", "-s", lsat.path, "/home/cmatthew/tmp/ls/"+lsat.filename]
-
-
-    bucket = lsat.pathrow
-
     return subprocess.call(command, cwd=lsat.location)
 
 
 #targets = ["/media/GEODATA/glc_landsat/GLCF.TSM.AZ-002.00.GLS2005/WRS2/"]
-targets = ["/media/GEODATA/glc_landsat/GLCF.TSM.AZ-002.00.GLS2005/WRS2/",
-                           "/mnt/nasy/Landsat7/GLS2005/WRS2/"]
+targets = ["/mnt/nasy/Landsat7/GLS2005/WRS2/",\
+           "/media/GEODATA/glc_landsat/GLCF.TSM.AZ-002.00.GLS2005/WRS2/"]
 sizelock = threading.Lock()
 total_size = 0
 orig_size = 0
@@ -83,7 +84,21 @@ for f in crawl_filesystem(targets):
     size = os.path.getsize(f)
     total_size += size
     to_upload.append(LandsatFile(f, size))
-    
+
+# are we going to start in the middle?
+spot = 0
+if len(sys.argv) > 1:
+    skip = sys.argv[1]
+    for ls in enumerate(to_upload):
+        if ls[1].pathrow == skip:
+            spot = ls[0]
+            break
+
+if len(sys.argv) > 1 and spot == 0:
+    print "Warning: didn't find restore point."
+print spot
+to_upload = to_upload[spot:]
+
 orig_size = total_size
 starttime = time.time()
 
@@ -92,10 +107,13 @@ nfiles = len(to_upload)
 ndone = 0
 print nfiles
 sizelock.release()
+
+sys.exit(1)
 print "Phase 2: Uploading"
 decimal.getcontext().prec = 4
 #for landsat in to_upload:
 #    upload_single_landsat(landsat)
+
 
 def done(job, rc):
     global total_size, sizelock, ndone
@@ -106,11 +124,11 @@ def done(job, rc):
     sizelock.release()
     currtime = time.time()
     elapsed = currtime - starttime
-    
+
     print "Remaining:", sizeof_fmt(total_size), \
-          "Speed:",sizeof_fmt((orig_size - total_size) / (elapsed)), \
-          "/s", (decimal.Decimal(ndone)/decimal.Decimal(nfiles))*100, "%"
-    
+          "Speed:", sizeof_fmt((orig_size - total_size) / (elapsed)), \
+          "/s", (decimal.Decimal(ndone) / decimal.Decimal(nfiles)) * 100, "%"
+
     if rc != 0:
         print "Error: failed to upload", job.args[0].filename
 
@@ -119,4 +137,3 @@ pool = threadpool.ThreadPool(10)
 requests = threadpool.makeRequests(upload_single_landsat, to_upload, done)
 [pool.putRequest(req) for req in requests]
 pool.wait()
-
