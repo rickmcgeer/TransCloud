9dc95f47a50413943e47e6c970f99879c3e134a6
Rick McGeer <rick@mcgeer.com>
2012-09-27 14:36:57 -0700

    bugfix

--- gisdemo/sqlQueries.py
+++ gisdemo/sqlQueries.py
@@ -25,7 +25,7 @@ class JSON_SQL_Query:
         self.geometry_column = geometry_column
 
 json_sql_queries = {'allCities':JSON_SQL_Query("SELECT map.name, map.greenspace, ST_AsGeoJSON(map.the_geom) FROM map order by map.greenspace desc limit 10000", ("name", "greenspace", "geom"), [0, 1], ("%s", "%f", ""), 2),
-                    'last10Cities':JSON_SQL_Query("SELECT map.name, map.greenspace, ST_AsGeoJSON(map.the_geom), processed.process_end_time FROM map INNER JOIN processed ON map.gid = processed.gid WHERE processed.file_path is not null order by processed.process_end_time desc limit 10;", [0, 1], ("name", "greenspace", "geom", "time"), ("%s", "%f", "" ""), 2)
+                    'last10Cities':JSON_SQL_Query("SELECT map.name, map.greenspace, ST_AsGeoJSON(map.the_geom), processed.process_end_time FROM map INNER JOIN processed ON map.gid = processed.gid WHERE processed.file_path is not null order by processed.process_end_time desc limit 10;",  ("name", "greenspace", "geom", "time"), [0, 1], ("%s", "%f", "" ""), 2)
     }
 
 try:
@@ -65,8 +65,8 @@ try:
         entries = []
         for item_number in range(0, len(resultTuple)):
             if item_number not in json_query.property_columns: continue
-            fmt_string = '"' + json_query.resultsNameTuple[item_number] + '":"' + json_query.fmtTuple[item_number] + '"'
-            entries += [fmt_string % resultTuple[itemNumber]]
+            fmt_string = '"' + json_query.resultsNameTuple[item_number] + '":"' + json_query.formatTuple[item_number] + '"'
+            entries += [fmt_string % resultTuple[item_number]]
         properties = ','.join(entries)
         response += properties + '},'
 	response += '"crs":{"type":"OGC", "properties":{"urn":"urn:ogc:def:crs:OGC:1.3:CRS84"}},'
