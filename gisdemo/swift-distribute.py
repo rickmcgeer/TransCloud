ab202368b80e8543513e9df156d3679ceb81c97d
Stephen Tredger <stredger@gmail.com>
2012-10-21 14:46:03 -0700

    modified to do it simpler, and syntax fix

--- gisdemo/swift-distribute.py
+++ gisdemo/swift-distribute.py
@@ -29,10 +29,10 @@ imglistlock = threading.Lock()
 
 alldownloadflag = 0
 
-# swift -A http://155.98.38.233:8080/auth/v1.0 -U system:gis -K uvicgis stat #emulab
-# swift -A http://142.104.195.225:8080/auth/v1.0 -U system:gis -K uvicgis stat #uvic
-# swift -A http://131.246.112.37:8080/auth/v1.0 -U system:gis -K uvicgis stat #glab
-# swift -A http://198.55.35.2:8080/auth/v1.0 -U system:gis -K uvicgis stat #geni orig!
+# swift -A http://155.98.38.233:8080/auth/v1.0 -U system:gis -K uvicgis stat #emulab site 1
+# swift -A http://142.104.195.225:8080/auth/v1.0 -U system:gis -K uvicgis stat #uvic site 2
+# swift -A http://131.246.112.37:8080/auth/v1.0 -U system:gis -K uvicgis stat #glab site 3
+# swift -A http://198.55.35.2:8080/auth/v1.0 -U system:gis -K uvicgis stat #geni orig! site 4
 
 
 def download_bucket(bucket, proxy):
@@ -49,7 +49,7 @@ def download_bucket(bucket, proxy):
         uplistlock.acquire()
         uplist.append(bucket)
         uplistlock.release()
-    except AssertioError:
+    except AssertionError:
         print "failed to download bucket!"
         dlfaillistlock.acquire()
         dlfaillist.append(bucket)
@@ -81,7 +81,7 @@ def upload_bucket(bucket, proxy):
         ret = swift.do_swift_command(proxy, "upload", bucket, "*")
         print ret.communicate()[0]
 
-    except AssertioError:
+    except AssertionError:
         print "failed to upload bucket!"
         upfaillistlock.acquire()
         upfaillist.append(bucket)
@@ -145,26 +145,33 @@ if __name__ == "__main__":
         print "temp swift dir already exists!"
     os.chdir('/tmp/swiftdist/')
 
-    # create download thread
-    dt = threading.Thread(target=dl_thread())
-    dlthreadlist.append(dt)
 
-    # create upload thread
-    ut = threading.Thread(target=up_thread())
-    upthreadlist.append(ut)
+    # screw the threading it doesnt alternate like i though it would
+    for b in dllist:
+        download_bucket(b)
+        upload_bucket(b)
 
-    # start threads
-    for dt in dlthreadlist:
-        dt.start()
-    for ut in upthreadlist:
-        ut.start()
 
-    # wait for threads to terminate
-    for dt in dlthreadlist:
-        dtt.join()
-        alldownloadflag = 1
-    for ut in upthreadlist:
-        ut.join()
+    # # create download thread
+    # dt = threading.Thread(target=dl_thread())
+    # dlthreadlist.append(dt)
+
+    # # create upload thread
+    # ut = threading.Thread(target=up_thread())
+    # upthreadlist.append(ut)
+
+    # # start threads
+    # for dt in dlthreadlist:
+    #     dt.start()
+    # for ut in upthreadlist:
+    #     ut.start()
+
+    # # wait for threads to terminate
+    # for dt in dlthreadlist:
+    #     dtt.join()
+    #     alldownloadflag = 1
+    # for ut in upthreadlist:
+    #     ut.join()
 
 
     print "FAILED DOWNLOADS"
