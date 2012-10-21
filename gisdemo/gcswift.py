7166172585f8eb35f8e34f1564c37c98ad9de274
Chris Matthews <chris4000@gmail.com>
2012-10-21 13:45:07 -0600

    swift works with clusters, and has a reupload option when download fails

--- gisdemo/gcswift.py
+++ gisdemo/gcswift.py
@@ -4,7 +4,6 @@ import signal
 import commands
 import subprocess
 import settings
-import optparse
 
 
 class Alarm(Exception):
@@ -47,24 +46,34 @@ def do_swift_command(swift_proxy, operation, bucket, timeout, *args):
   return p
 
 
-DEFAULT_SWIFT_HOST = "198.55.35.2"
 
 def swift(operation, bucket, *args):
   """operation: upload, download
   bucket: bucket number
   args: files needed to download
   """
-  parser = optparse.OptionParser()
-  parser.add_option("-s", "--swift_host", dest="swift_host", type="string", default=DEFAULT_SWIFT_HOST, help="hostname or ip of the swift server")
-  (options, args_not_used) = parser.parse_args()
-  print "Using swift proxy http://"+options.swift_host+":8080/auth/v1.0"
+  # if the image is migging, upload it
+  cache = False
 
-  process = do_swift_command("http://"+options.swift_host+":8080/auth/v1.0", operation, bucket, True, *args)
-  if process.returncode != 0 and options.swift_host != DEFAULT_SWIFT_HOST:
-    print "Failed on swift host %s with %s, trying on %s" % (options.swift_host, process.communicate()[1], DEFAULT_SWIFT_HOST)
-    process = do_swift_command(settings.SWIFT_PROXY, operation, bucket, True, *args)
-  assert process.returncode == 0, "Failed on swift host %s with %s" % (DEFAULT_SWIFT_HOST, process.communicate()[1])
+  process = do_swift_command("http://"+settings.SWIFT_PROXY1+":8080/auth/v1.0", \
+                                 operation, bucket, True, *args)
+  if process.returncode != 0:
+    message = process.communicate()[1]
+    print "Warning: Failed on swift host %s with %s, trying on %s" % \
+        (settings.SWIFT_PROXY1, message, settings.SWIFT_PROXY2)
+    if operation == "download" and "Object" in message and "not found" in message:
+        cache = True
 
+
+    process = do_swift_command("http://"+settings.SWIFT_PROXY2+":8080/auth/v1.0", operation, bucket, True, *args)
+  assert process.returncode == 0, "Failed on swift host %s with %s" % \
+      (settings.SWIFT_PROXY2, process.communicate()[1])
+  if cache:
+      print "Uploading the image to local cluster..."
+      process = do_swift_command("http://"+settings.SWIFT_PROXY1+":8080/auth/v1.0", "upload", bucket, True, *args)
+      if process.returncode != 0:
+          message = process.communicate()[1]
+          print "Uploading failed." + message
 
 if __name__ == '__main__':   
   swift("download", "p233r094", 'p233r094_7dt20040310.SR.b03.tif.gz', 'p233r094_7dt20040310.SR.b04.tif.gz')
