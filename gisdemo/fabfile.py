b9c66f6f747ecba4ee0aa38eb0bcbbda0dd79886
Rick McGeer <rick@mcgeer.com>
2013-03-03 15:49:48 -0800

    fixed code for server_deploy

--- gisdemo/fabfile.py
+++ gisdemo/fabfile.py
@@ -143,21 +143,13 @@ def install_deps():
 
 @roles('db_server')
 def server_deploy():
-    all_tables = ['by_continent', 'by_continent_id_seq', 'ca', 'ca_cities', 'ca_province',
-                   'ca_province_gid_seq', 'cgi_query_values_history',
-                   'cgi_query_values_history_query_id_seq', 'geography_columns', 'geometry_columns',
-                   'map', 'map_gid_seq', 'processed', 'spatial_ref_sys', 'tiff4326', 'tiff4326_id_seq',
-                   'us', 'us_cities', 'us_state', 'us_state_gid_seq']
-    writeable_tables =  ['cgi_query_values_history', 'cgi_query_values_history_query_id_seq', 'map',
-                         'map_gid_seq', 'processed']
-    sudo('createdb ' + settings.GIS_DATABASE, user="postgres")
-    for user in ['root', 'www-data', 'gis']:
-        sudo('createuser ' + user, user='postgres')
-    sudo('psql ' + settings.GIS_DATABASE + ' < ' + deploy_path +'/clean_world.sql', user='postgres')
-    for relation in all_tables:
-        sudo('echo "grant read on ' +relation + 'to public;" | psql ' + settings.GIS_DATABASE)
-    for relation in writeable_tables:
-        sudo('echo "grant all on ' +relation + 'to public;" | psql ' + settings.GIS_DATABASE)
+    database = 'world'
+    with settings(warn_only=True):
+        sudo('createdb ' + database, user="postgres")
+        for user in [ 'www-data', 'gis']:
+            sudo('createuser -S -R -D ' + user, user='postgres')
+        sudo('createuser -s -r -d root', user='postgres')
+        sudo('psql ' + database + ' -f ' + deploy_path +'/clean_world.sql', user='postgres')
 
 @roles('server')
 def run_start():
